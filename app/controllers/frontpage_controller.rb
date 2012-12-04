f3c185f1603a670b7cbfff1ab014a7d5e6b28133
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2012-12-04 16:05:53 -0200

    First version of the new frontpage customized for UFRGS, refs #451

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -34,47 +34,47 @@ class FrontpageController < ApplicationController
 #    #recent_events = The upcoming events in public spaces
 #    @recent_events = Event.find(:all, :order => "start_date Desc").select{|p| !p.space.disabled? && p.space.public? &&  p.start_date && p.start_date.future?}.first(2)
 
-    # Fetch the meetings running in the server and order by participant_count
-    # TODO Temporarily using the first server
-    @stats = {}
-    @running_spaces = []
-    @popular_spaces = []
+    # # Fetch the meetings running in the server and order by participant_count
+    # # TODO Temporarily using the first server
+    # @stats = {}
+    # @running_spaces = []
+    # @popular_spaces = []
 
-    @server = BigbluebuttonServer.first
-    begin
-      @server.fetch_meetings
+    # @server = BigbluebuttonServer.first
+    # begin
+    #   @server.fetch_meetings
 
-      # statistics
-      @stats[:meetings] = @server.meetings.select { |m| m.is_running? }.count
-      @stats[:users] = User.count
-      @stats[:spaces] = Space.count
+    #   # statistics
+    #   @stats[:meetings] = @server.meetings.select { |m| m.is_running? }.count
+    #   @stats[:users] = User.count
+    #   @stats[:spaces] = Space.count
 
-      # spaces with webconferences running
-      spaces_ids = @server.meetings.select{ |m| m.owner_type == "Space" }.map{ |m| m.owner_id }
-      @running_spaces = Space.find_all_by_id(spaces_ids).select{ |s| s.public? }
-      @running_spaces.each do |space|
-        space.bigbluebutton_room.fetch_meeting_info
-      end
-      @running_spaces.sort! { |x,y| y.bigbluebutton_room.participant_count <=> x.bigbluebutton_room.participant_count }
-    rescue Exception
-    end
+    #   # spaces with webconferences running
+    #   spaces_ids = @server.meetings.select{ |m| m.owner_type == "Space" }.map{ |m| m.owner_id }
+    #   @running_spaces = Space.find_all_by_id(spaces_ids).select{ |s| s.public? }
+    #   @running_spaces.each do |space|
+    #     space.bigbluebutton_room.fetch_meeting_info
+    #   end
+    #   @running_spaces.sort! { |x,y| y.bigbluebutton_room.participant_count <=> x.bigbluebutton_room.participant_count }
+    # rescue Exception
+    # end
 
-    # Get the spaces with more users and fetch info about their webconference rooms
-    @popular_spaces = Space.public.sort_by{|s| s.users.size}.reverse.first(3)
-    @popular_spaces.each do |space|
-      begin
-        space.bigbluebutton_room.fetch_meeting_info
-      rescue Exception
-      end
-    end
+    # # Get the spaces with more users and fetch info about their webconference rooms
+    # @popular_spaces = Space.public.sort_by{|s| s.users.size}.reverse.first(3)
+    # @popular_spaces.each do |space|
+    #   begin
+    #     space.bigbluebutton_room.fetch_meeting_info
+    #   rescue Exception
+    #   end
+    # end
 
     respond_to do |format|
       if logged_in?
         format.html { redirect_to home_path}
       else
-        format.html # index.html.erb
-        format.xml  { render :xml => @spaces }
-        format.atom
+        format.html { render :index, :layout => "frontpage_ufrgs" }
+        # format.xml  { render :xml => @spaces }
+        # format.atom
       end
     end
   end
